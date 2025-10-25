b20afd41d13aa1de1e34959d9ab584ebf8c60e99
aartemkin <artemkin030@gmail.com>
2025-10-25 16:10:22 +0400

    Added a model and migration for user wallets

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Currency extends Model
 {
@@ -10,4 +11,9 @@ class Currency extends Model
         'code',
         'name',
     ];
+
+    public function wallets(): HasMany
+    {
+        return $this->hasMany(Wallet::class);
+    }
 }
---
+++ app/Models/Wallet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Wallet extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'currency_id',
+        'balance',
+    ];
+
+    public function currency(): BelongsTo
+    {
+        return $this->belongsTo(Currency::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
